0c6874831025d3bfaf9e9bc7ae42ed46a151ed74
saad-official <tommmyy890@gmail.com>
2024-05-17 06:35:02 +0500

    adding feature

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,14 +1,29 @@
+"use client";
 import { ReactNode } from "react";
 import MainMenu from "./components/main-menu";
 import MenuTitle from "./components/menu-title";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import { MenuIcon } from "lucide-react";
+import { useMediaQuery } from "@/hooks/use-media-query";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+  const isDesktop = useMediaQuery("(min-width:768px)");
   return (
     <div className="grid md:grid-cols-[250px_1fr] h-screen">
       <MainMenu className="hidden md:flex" />
-      <div className="p-4 block md:hidden sticky top-0 left-0 bg-background border-b  border-border ">
-        <MenuTitle />
-      </div>
+      {!isDesktop && (
+        <div className="p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b  border-border ">
+          <MenuTitle />
+          <Drawer>
+            <DrawerTrigger>
+              <MenuIcon />
+            </DrawerTrigger>
+            <DrawerContent>
+              <MainMenu />
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
       <div className="overflow-auto px-4 py-2">
         <h1 className="pb-4">Welcome back, Tom!</h1>
         {children}
